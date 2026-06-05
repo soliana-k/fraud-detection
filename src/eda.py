b226b955eeb7ee31e7798ebd2e562297ea05d97a
kalkidan <kalukassahun29@gmail.com>
2026-06-05 13:55:25 +0300

    chore: intialize environment-setup, create a requirement.txt containing packages needed, add gitignore respective folders and files needed to be ignored

---
+++ src/eda.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder
+
+class Eda:
+    def __init__(self):
+        pass
+
+    def load_data(self):
+        pass
+
+    
